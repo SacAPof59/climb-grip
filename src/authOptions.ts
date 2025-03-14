5089236bc6f4522f4ad6317f849f9696cefa3712
Kek-Desktop <kevin.arnaud.addpro@gmail.com>
2025-03-14 13:28:22 +0100

    ci: adding check on pr

--- src/authOptions.ts
+++ src/authOptions.ts
@@ -1,27 +1,26 @@
 import { NextAuthOptions } from 'next-auth';
-import {PrismaAdapter} from "@auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import {PrismaClient} from "@prisma/client";
-
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    session: {
-        strategy: 'jwt',
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
     },
-    callbacks: {
-        async session({ session, token }) {
-            if (session?.user) {
-                session.user.id = token.sub;
-            }
-            return session;
-        },
-    },
+  },
 };
